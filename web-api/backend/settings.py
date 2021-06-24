046c3bfc43285e30c1c16b504a55e0afacd1ddd1
McMenemy <josh.mcmenemy@gmail.com>
2021-06-24 03:46:00 -0700

    update jwt settings

--- web-api/backend/settings.py
+++ web-api/backend/settings.py
@@ -143,8 +143,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=45),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
